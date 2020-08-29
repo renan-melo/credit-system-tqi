2a962b6e26c7fe803ce015de79e0db0cc22dcd32
Renandev2020 <renanhiatus@gmail.com>
2020-08-29 19:35:17 -0300

    init

--- credit-system-tqi/src/main.js
+++ credit-system-tqi/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+ 
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
